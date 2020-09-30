b96261106857d3567b652c3a8c244fb8094bd24a
Marcelo2m97 <marcelo2m1097@gmail.com>
2020-09-29 22:44:01 -0600

    Model for User created.

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const crypto = require('crypto')
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    hashedPassword: {
+        type: String,
+        default: "",
+        required: true
+    },
+});
+
+UserSchema.
+    virtual("password")
+    .set(function(password){
+        this.hashedPassword = Crypto.createHmac("sha256", password).diggest("hex");
+    });
+
+UserSchema.methods = {
+    comparePassword: function(password) {
+        return (Crypto.createHmac("sha256", password).diggest("hex") === this.hashedPassword)
+    }
+}
+
+module.exports = mongoose.model("User", UserSchema);
