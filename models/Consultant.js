8c1fa34274be11eebd85c2bcd53bda9a98caaf8d
Marcelo2m97 <marcelo2m1097@gmail.com>
2020-09-30 17:03:21 -0600

    Consultant model was created.

---
+++ models/Consultant.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ConsultantSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    birthdate: {
+        type: Date,
+        required: true
+    },
+    referencePrice: {
+        type: Boolean,
+        required: true
+    },
+    historicAveragePrice: {
+        type: Boolean,
+        required: true
+    },
+    phoneNumber: {
+        type: String,
+        required: true
+    },
+    averageRating: {
+        type: Boolean,
+        default: 0.00
+    },
+    consultantType: {
+        type: [{
+            type: mongoose.Schema.Types.ObjectId,
+            rel: ConsultantType
+        }]
+    }
+});
