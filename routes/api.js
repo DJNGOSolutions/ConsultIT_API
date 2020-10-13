d89d774747a0a735e008eed00d2f868e03afc396
Marcelo2m97 <marcelo2m1097@gmail.com>
2020-10-13 16:15:56 -0600

    States can be added.

--- routes/api.js
+++ routes/api.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const UserRouter = require("./api/User");
 const CityRouter = require("./api/City");
+const StateRouter = require("./api/State");
 
 router.use('/user', UserRouter);
 router.use('/city', CityRouter);
+router.use('/state', StateRouter);
 
 module.exports = router;
