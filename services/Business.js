a5cb14476a7091d3216060a247adcd8267ce5d47
Nelson Castro <00043516@uca.edu.sv>
2020-10-15 14:30:01 -0600

    Adding setter in Business Service

--- services/Business.js
+++ services/Business.js
@@ -13,4 +13,48 @@ BusinessService.test = () => {
     return serviceResponse;
 };
 
+BusinessService.createNewBusiness = async ({ legalName, comercialName, email, phoneNumber, address, state, city, businessLine, businessSector }) => {
+
+    let serviceResponse = {
+        success: true,
+        content: {}
+    }
+
+    if(!legalName || !comercialName ||!email || !phoneNumber || !address || !state || !city || !businessLine || !businessSector){
+        serviceResponse={
+            success: false,
+            content:{
+                error: "A required field was not provided"
+            }
+        }
+        return serviceResponse;
+    }
+
+    try{
+        const newBusiness = new BusinessModel({legalName, comercialName, email, phoneNumber, address,state, city, businessLine, businessSector});
+        console.log("New Business Model: " + newBusiness);
+        const savedBusiness = await newBusiness.save();
+        if (!savedBusiness) {
+            serviceResponse = {
+                success: false,
+                content: {
+                    error: "Business could not be registered"
+                }
+            }     
+            return serviceResponse;
+        }
+        
+        serviceResponse.content = {
+            message: "A new Business has been registered"
+        }
+
+        return serviceResponse;
+        
+    }catch(error){
+        console.log("An error occurred" + error);
+        throw new Error("Internal Server Error");
+    }
+
+};
+
 module.exports = BusinessService;
