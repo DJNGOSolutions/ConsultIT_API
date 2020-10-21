15a1aa01a9fd171e990bad35c088da5be67fa1bc
Marcelo2m97 <marcelo2m1097@gmail.com>
2020-10-21 15:05:19 -0600

    Creation of business line is available.

--- routes/api.js
+++ routes/api.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const BusinessRouter = require("./api/Business");
+const BusinessLineRouter = require("./api/BusinessLine");
 const UserRouter = require("./api/User");
 const CityRouter = require("./api/City");
 const StateRouter = require("./api/State");
 
 router.use('/business', BusinessRouter);
+router.use('/businessLine', BusinessLineRouter);
 router.use('/user', UserRouter);
 router.use('/city', CityRouter);
 router.use('/state', StateRouter);
