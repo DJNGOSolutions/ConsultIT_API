b8a1439d4d11aa7d8664fa07d20f2e4882dfd393
Nelson Castro <00043516@uca.edu.sv>
2020-11-15 17:42:54 -0600

    Added debug method for User

--- routes/api/User.js
+++ routes/api/User.js
@@ -3,7 +3,11 @@ const router = express.Router();
 
 const UserController = require('../../controllers/api/User');
 
+router.get("/findAll", UserController.findAll);
+
 router.post('/signup', UserController.register);
 router.post('/signin', UserController.login);
+
+router.delete("/deleteByID", UserController.deleteByID);
 
 module.exports = router;
