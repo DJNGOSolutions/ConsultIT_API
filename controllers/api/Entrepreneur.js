debcd793deb5652131ae7c054de47fbfebc08a6a
Marcelo2m97 <marcelo2m1097@gmail.com>
2020-11-21 18:22:40 -0600

    Find entrepreneur by params instead of body

--- controllers/api/Entrepreneur.js
+++ controllers/api/Entrepreneur.js
@@ -7,7 +7,7 @@ const BusinessService = require('../../services/Business');
 const EntrepreneurController = {};
 
 EntrepreneurController.findOneEntrepreneurByUser = async (req, res) => {
-    let { username } = req.body;
+    let { username } = req.query;
 
     if(!username){
         return res.status(403).json({
