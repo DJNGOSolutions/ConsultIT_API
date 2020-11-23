9cf769280f74c37738fc0ee37f4c068decee8a59
Nelson Castro <00043516@uca.edu.sv>
2020-11-22 22:01:53 -0600

    Added update to Business

--- services/Business.js
+++ services/Business.js
@@ -97,6 +97,32 @@ BusinessService.createNewBusiness_Entrepreneur = async (legalName, comercialName
 
 };
 
+BusinessService.findOneBusinessByUser = async (_id) => {
+    let serviceResponse = {
+        success: true,
+        content: {
+            message: "El Business fue encontrado"
+        }
+    }
+    
+    try{
+        const business = await BusinessModel.findOne({ user: _id });
+        if (!business) {
+            serviceResponse  = {
+                success: false,
+                content: {
+                    error: "El Business no se encontró"
+                }
+            }
+        } else {
+            serviceResponse.content = business;
+        }
+        return serviceResponse;
+    } catch(error) {
+        throw new Error("Internal Server Error.");
+    }
+};
+
 BusinessService.findAll = async () => {
     let serviceResponse ={
         success: true,
@@ -149,6 +175,67 @@ BusinessService.deleteOneByID = async (_id) => {
     }catch(error){
         console.log("An error occurred: " + error);
         throw new Error("Internal Server Error");
+    }
+};
+
+BusinessService.verifyUpdateFields = ({ legalName, comercialName, email, phoneNumber, address, state, city, businessLine, businessSector }) => {
+    let serviceResponse = {
+        success: true,
+        content: {}
+    }
+    
+    if (!legalName && !comercialName && !email && !phoneNumber && !address && !state && !city && !businessLine && !businessSector){
+        serviceResponse = {
+            success: false,
+            content: {
+                error: "No se especifico ningún campo para actualizar"
+            }
+        }
+        
+        return serviceResponse;
+    }
+    
+    if(legalName) serviceResponse.content.legalName = legalName;
+    if(comercialName) serviceResponse.content.comercialName = comercialName;
+    if(email) serviceResponse.content.email = email;
+    if(phoneNumber) serviceResponse.content.phoneNumber = phoneNumber;
+    if(address) serviceResponse.content.address = address;
+    if(state) serviceResponse.content.state = state;
+    if(city) serviceResponse.content.city = city;
+    if(businessLine) serviceResponse.content.businessLine = businessLine;
+    if(businessSector) serviceResponse.content.businessSector = businessSector;
+    
+    return serviceResponse;
+};
+
+BusinessService.updateConsultantById = async (business, newBusinessData) => {
+    let serviceResponse = {
+        success: true,
+        content: {
+            message: "El Negocio se ha actualizado"
+        }
+    }
+    
+    try{
+        Object.keys(newBusinessData).forEach(key => {
+            business[key] = newBusinessData[key];
+        });
+        const updatedBusiness = await business.save()
+
+        if(!updatedBusiness) {
+            serviceResponse = {
+                success: false,
+                content: {
+                    error: "El Negocio no se pudo actualizar"
+                }
+            }
+        } else {
+            serviceResponse.content = updatedBusiness;
+        }
+
+        return serviceResponse;
+    } catch(error) {
+        throw new Error("Internal Server Error.")
     }
 };
 
